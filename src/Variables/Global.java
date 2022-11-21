c0791263e4246f8ee5798f1ae3c06866aa10aebc
ecuellarfernandez <ecuellarfernandez@gmail.com>
2022-11-20 21:59:44 -0400

    Creada clase para variables globales

--- src/Variables/Global.java
+++ src/Variables/Global.java
@@ -1,7 +1,12 @@
+package Variables;
+
 import java.util.ArrayList;
 
 public class Global {
-    static String[] datosRelojes = {"America/La_Paz"};
-    static final int anchoVentana = 500;
-    static final int altoVentana = 300;
+    public static ArrayList<String> datosRelojes = new ArrayList<String>(){{add("America/La_Paz");}};
+    public static final int anchoVentana = 500;
+    public static final int altoVentana = 300;
+    public static final int anchoVentanaSec = 350;
+    public static final int altoVentanaSec = 150;
+
 }
