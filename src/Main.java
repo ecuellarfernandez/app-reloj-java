6cbb618544811fd52f89b2903f040f170d15cc37
ecuellarfernandez <ecuellarfernandez@gmail.com>
2022-11-21 00:52:36 -0400

    Agregado nuevo look and feel

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,11 @@
 import Ui.Inicio;
 import Variables.Global;
+import com.formdev.flatlaf.*;
+import javax.swing.*;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws UnsupportedLookAndFeelException {
+        UIManager.setLookAndFeel(new FlatIntelliJLaf());
         new Inicio(Global.datosRelojes);
     }
 }
