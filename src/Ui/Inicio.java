5615de79a4312fc68b09dfcbfc3fb5af0934a6df
ecuellarfernandez <ecuellarfernandez@gmail.com>
2022-11-20 21:58:00 -0400

    Cambio datosRelojes pasa a ser ArrayList para manipular mejor los datos, Añadido listener al presionar boton añadir crea nueva ventana para crear nuevo reloj

--- src/Ui/Inicio.java
+++ src/Ui/Inicio.java
@@ -1,30 +1,49 @@
 package Ui;
 
+import Variables.Global;
+
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 public class Inicio extends JFrame{
     private JPanel mainPanel, body;
-    private JLabel lblTitulo;
     private JButton btnEditar, btnAdd;
-    private String[] datosRelojes;
+    private ArrayList<String> datosRelojes;
 
-    public Inicio(String[] datosRelojes){
+    public Inicio(ArrayList<String> datosRelojes){
+        Inicio pantallaInicio = this;
         this.datosRelojes = datosRelojes;
         setVisible(true);
-        setSize(500, 300);
+        setSize(Global.anchoVentana, Global.altoVentana);
         setLocationRelativeTo(null);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
         add(mainPanel);
 
         //LAYOUT
         body.setLayout(new BoxLayout(body, BoxLayout.Y_AXIS));
 
         imprimirRelojes();
+
+        //Listener para el boton de añadir reloj
+        btnAdd.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                //Creo la ventana para añadir reloj
+                //Envío como parámetro la pantalla actual para manipularla en la ventana de añadir reloj
+                new VentanaAddReloj(pantallaInicio);
+            }
+        });
     }
 
+    //Imprimir relojes obteniendo los datos del array recibido por parametro
     private void imprimirRelojes() {
-        for (int i = 0; i < datosRelojes.length; i++) {
-            body.add(new PanelReloj(datosRelojes[i]));
+        for (int i = 0; i < datosRelojes.size(); i++) {
+            //Añadimos al body del mainPanel los paneles de los relojes
+            body.add(new PanelReloj(datosRelojes.get(i)));
         }
 
     }
